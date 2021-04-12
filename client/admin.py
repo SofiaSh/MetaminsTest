3e2b286957441af909f1ba9dfc50da11f9765ecb
sofia <79996200981@ya.ru>
2021-04-12 10:38:57 +0700

    admin panel customization

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    search_fields = ['phone_number', 'card_number', 'last_name']
+    list_display = ['phone_number']
+    fields = ['first_name', 'last_name', 'phone_number']
+
+
+admin.site.register(Client, ClientAdmin)
--- client/models.py
+++ client/models.py
@@ -8,11 +8,11 @@ class Client(AbstractUser):
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
-    card_number_regex = RegexValidator(regex=r'\d+',
+    card_number_regex = RegexValidator(regex=r'[0-9]{16}',
                                        message='Wrong card number')
     card_number = models.CharField(validators=[card_number_regex], max_length=16)
 
-    balance = models.IntegerField()
+    balance = models.IntegerField(default=0)
 
     def __str__(self):
         return self.username
